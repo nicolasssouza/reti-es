355b07cd8024f62cb3aa2efc4a85054680e1deb6
Nícolas Santana <nicolas.s.souza14@aluno.senai.br>
2026-06-16 16:41:18 -0300

    Modificações 1 no js/scripts_repeticoes.js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,4 +1,12 @@
 //ESTRUTURA DE REPETIÇÃO FOR
+const divfor = document.querySelector('#div-for')
+
 for(i = 0;i < 10; i++){
     console.log(i, 'Amor')
+    divfor.innerHTML += `${i} - Amor <br>`
 }
+
+//CONTADOR/ACUMULADOR
+const inputNum = document.querySelector('#num')
+const btnNum = document.querySelector('#btn-num')
+const divContAcum = document.querySelector('#div-cont-acum')
