fd47cf76846be74462ce33a74de1ae09346ee85a
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-17 13:03:08 -0300

    modificações no js/scripts_repeticoes.js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -10,3 +10,15 @@ for(i = 0;i < 10; i++){
 const inputNum = document.querySelector('#num')
 const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
+
+let cont = 0
+let acum = 0.0
+
+btnNum.addEventListener('click' , (evt)=>{
+    let numDigitado = Number(inputNum.value)
+
+    cont++
+    acum += numDigitado
+
+    divContAcum.innerHTML = `TOTAL DE NÚMERO DIGITADO É: ${cont}`
+})
