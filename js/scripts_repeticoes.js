cfd02fdfb3c275e4cdb1c90bf0a6b57d4d864fdc
Nícolas Santana <nicolas.s.souza14@aluno.senai.br>
2026-06-17 16:25:35 -0300

    Modificações 5  js/scripts_repeticoes.js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -73,3 +73,12 @@ const divForeach = document.querySelector('#div-foreach')
 presentes.forEach((elemento, posicao)=>{
     divForeach.innerHTML += `${posicao} - ${elemento} <br>`
 })
+
+//COLEÇÂO DE OBJETOS LITERAIS - ARRAY
+const pessoas = [
+    {nome: 'Maria Flor', idade: 25, renda:8500},
+    {nome: 'Joerdson Souza', idade: 75, renda:5000},
+    {nome: 'Taoca', idade: 28, renda:500},
+    {nome: 'Chicó', idade: 36, renda:100},
+    {nome: 'João Grilo', idade: 32, renda:80},
+]
