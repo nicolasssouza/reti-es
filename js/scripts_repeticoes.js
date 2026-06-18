81ccd9a2fad12acf09d6ee5c0b9fabf3b1acfeea
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-18 15:42:53 -0300

    Modificações feitas no js/scripts_repeticoes.js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -89,3 +89,31 @@ const divListaObjFor = document.querySelector('#div-listaobj-for')
 for(i = 0; i < pessoas.length;i++){
     divListaObjFor.innerHTML += `${pessoas[i].nome}, ${pessoas[i].idade} R$ ${pessoas[i].renda.toFixed(2).replace('.', ',')} <br>`
 }
+
+//LISTANDO OBJETO LITERAL PELO FOR/IN
+const divListaObjForIn = document.querySelector('#div-listaobj-forin')
+
+for(let indice in pessoas){
+    divListaObjForIn.innerHTML += `${indice} - ${pessoas
+    [indice].nome}, ${pessoas[indice].idade}, R$ ${pessoas[indice].renda.toFixed(2).replace('.',',')} <br>`
+
+}
+
+//LISTANDO OBJETO LITERAL PELO FOR/OF
+const divListaObjForOF =document.querySelector('#div-listaobj-forof')
+
+let contForOf = 0
+
+for(let elem of pessoas){
+    contForOf++
+
+    divListaObjForOF.innerHTML += `${contForOf} - ${elem.nome}, ${elem.idade}, R$ ${elem.renda.toFixed(2).replace('.',',')} <br>`
+}
+
+//LISTANDO OBJETO LITERAL PELO FOREACH
+const divListaForeach = document.querySelector
+('#div-lista-foreach')
+
+pessoas.forEach((elem, i)=>{
+    divListaForeach.innerHTML += `${i + 1} - ${elem.nome}, ${elem.idade} R$ ${elem.renda.toFixed(2).replace('.',',')}<br>`
+})
