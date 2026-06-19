311de4ffee130e0c94d856d53728dc9649fce0f3
Nícolas Santana <nicolas.s.souza14@aluno.senai.br>
2026-06-19 16:40:54 -0300

    atualizado modified:   js/scripts_calculos.js

---
+++ js/scripts_calculos.js
@@ -0,0 +1,21 @@
+/**
+ * VALOR FOR ATÉ 100 O CÁLCULO SEJA 10%
+ * VALRO FOR DE 101 ATÉ 1000 O CÁCULO SEJA 15%
+ * ACIMA DE 1000 SEJA DE O CÁLCULO SEJA 20%
+ */
+
+const calculoPercentual = (valor) => {
+    let valorCalculado = 0.0
+
+    if (valor <= 100){
+        valorCalculado = valor * 0.10
+    }else if( valor <= 1000){
+        valorCalculado = valor * 0.15
+    }else{
+        valorCalculado = valor * 0.20
+    }
+
+    return valorCalculado
+}
+
+export{calculoPercentual}
